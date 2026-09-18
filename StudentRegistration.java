722bdc3d4f4cea5740a9decf5fcf69c313580563
sanskritigupta0906-coder <sanskritigupta0906@gmail.com>
2026-09-18 11:59:08 +0530

    Add files via upload

---
+++ StudentRegistration.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class StudentRegistration {
+
+    public static boolean registerStudent(Set<Integer> ids, int id) {
+        // Write your code
+        if(ids.contains(id))
+            return false;
+        
+        ids.add(id);
+        return true;
+    }
+
+    public static boolean isRegistered(Set<Integer> ids, int id) {
+        // Write your code
+        return ids.contains(id);
+    }
+
+    public static int totalStudents(Set<Integer> ids) {
+        // Write your code
+        return ids.size();
+    }
+
+    public static void main(String[] args) {
+
+        Set<Integer> ids = new HashSet<>();
+
+        System.out.println(registerStudent(ids, 101));
+        System.out.println(registerStudent(ids, 102));
+        System.out.println(registerStudent(ids, 103));
+        System.out.println(registerStudent(ids, 101));
+
+        System.out.println("Registered: "
+                + isRegistered(ids, 102));
+
+        System.out.println("Total Students: "
+                + totalStudents(ids));
+    }
+}
